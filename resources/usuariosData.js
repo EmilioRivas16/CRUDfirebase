ac65dd0b18e00cb6cb03b69ee684456b2c837d2e
EmilioRivas16 <emiliorivas16@hotmail.com>
2023-04-28 11:40:23 -0600

    Tarea1Parcial3NuevasTec

---
+++ resources/usuariosData.js
@@ -0,0 +1,37 @@
+export const users = [
+    {
+        id: 1,
+        name: 'Abelardo',
+        role: 'Developer',
+        address: 'abc123',
+        salary: 1000,
+    },
+    {
+        id: 2,
+        name: 'Rodrigo',
+        role: 'Support',
+        address: 'abc123',
+        salary: 2000,
+    },
+    {
+        id: 3,
+        name: 'Alberto',
+        role: 'Developer',
+        address: 'abc123',
+        salary: 3000,
+    },
+    {
+        id: 4,
+        name: 'Pedro',
+        role: 'Marketing',
+        address: 'abc123',
+        salary: 4000,
+    },
+    {
+        id: 5,
+        name: 'Maria',
+        role: 'QA',
+        address: 'abc123',
+        salary: 5000,
+    },
+];
